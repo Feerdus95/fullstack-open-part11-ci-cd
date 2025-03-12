0b515f7f79666c5c74438bf448197a84817ff91a
Feerdus95 <ferdus95.fv@gmail.com>
2025-03-12 17:51:53 -0300

    exercises 11.1 - 11.9 working locally

--- fullstack-open-part11-pokedex/e2e-tests/pokedex.spec.js
+++ fullstack-open-part11-pokedex/e2e-tests/pokedex.spec.js
@@ -2,13 +2,13 @@ const { test, expect } = require('@playwright/test')
 
 test.describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:5000')
+    await page.goto('/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 
   test('pokemon page can be navigated to', async ({ page }) => {
-    await page.goto('http://localhost:5000')
+    await page.goto('/')
     await page.click('text=ivysaur')
     await expect(page.getByText('chlorophyll')).toBeVisible()
   })
--- fullstack-open-part11-pokedex/playwright.config.js
+++ fullstack-open-part11-pokedex/playwright.config.js
@@ -1,7 +1,30 @@
-module.exports = {
+const { defineConfig, devices } = require('@playwright/test')
+
+module.exports = defineConfig({
+  testDir: './e2e-tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:5000',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
   webServer: {
-    command: 'npm run start',
+    command: 'npm run build && npm run start-prod',
     port: 5000,
     timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
   },
-}
+})
